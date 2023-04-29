14c35826ba2af5e2855fe51d72097ae4bacc58ed
Vanderlei Kleinschmidt <vanderleik@yahoo.com.br>
2023-04-29 11:18:38 -0300

    Criando conexão com o banco de dados

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # adega-de-vinhos
  Adega de vinhos com Spring Boot
+
+**Projeto em construção**
+
+<h3>Algumas informações enquanto crio o projeto:</h3>
+Estou usando o Docker para subir um container do MySql. Assim não preciso rodar o MySql localmente.
+Estou usando o DBeaver para gerenciar o banco de dados.
--- src/main/java/adega/de/vinhos/adegadevinhos/domain/Vinho.java
+++ src/main/java/adega/de/vinhos/adegadevinhos/domain/Vinho.java
@@ -2,11 +2,21 @@ package adega.de.vinhos.adegadevinhos.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @AllArgsConstructor
 @Data
+@Entity
+@NoArgsConstructor
 public class Vinho {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String tipo;
 }
