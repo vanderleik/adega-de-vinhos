b2cd963a544ef7617226878d26455979a180a2ca
Vanderlei Kleinschmidt <vanderleik@yahoo.com.br>
2023-04-29 09:41:25 -0300

    Criando o método POST

--- src/main/java/adega/de/vinhos/adegadevinhos/controller/VinhoController.java
+++ src/main/java/adega/de/vinhos/adegadevinhos/controller/VinhoController.java
@@ -5,11 +5,9 @@ import adega.de.vinhos.adegadevinhos.service.VinhoService;
 import adega.de.vinhos.adegadevinhos.util.DateUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -32,5 +30,10 @@ public class VinhoController {
     @GetMapping(path = "/{id}")
     public ResponseEntity<Vinho> findById(@PathVariable long id) {
         return ResponseEntity.ok(vinhoService.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Vinho> save(@RequestBody Vinho vinho){
+        return new ResponseEntity(vinhoService.save(vinho), HttpStatus.CREATED);
     }
 }
--- src/main/java/adega/de/vinhos/adegadevinhos/service/VinhoService.java
+++ src/main/java/adega/de/vinhos/adegadevinhos/service/VinhoService.java
@@ -5,15 +5,21 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Service
 public class VinhoService {
 
-    private List<Vinho> vinhos = List.of(new Vinho(1L, "Branco"),
-            new Vinho(2L, "Tinto"),
-            new Vinho(3L, "Rose"),
-            new Vinho (4L, "Espumante"));
+    private static List<Vinho> vinhos;
+
+    static {
+        vinhos = new ArrayList<>(List.of(new Vinho(1L, "Branco"),
+                new Vinho(2L, "Tinto"),
+                new Vinho(3L, "Rose"),
+                new Vinho(4L, "Espumante")));
+    }
 
     public List<Vinho> listAll() {
         return vinhos;
@@ -21,5 +27,11 @@ public class VinhoService {
 
     public Vinho findById(long id) {
         return vinhos.stream().filter(vinho -> vinho.getId().equals(id)).findFirst().orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Vinho não encontrado"));
+    }
+
+    public Vinho save(Vinho vinho) {
+        vinho.setId(ThreadLocalRandom.current().nextLong(5, 1000));
+        vinhos.add(vinho);
+        return vinho;
     }
 }
