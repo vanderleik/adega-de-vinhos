2008b7b23deaa3905fcb3f4a4200ffb05fee2287
vanderleik <vanderleik@yahoo.com.br>
2023-05-01 21:26:29 -0300

    Testes unitários com H2 para repository

--- src/main/java/adega/de/vinhos/adegadevinhos/domain/Vinho.java
+++ src/main/java/adega/de/vinhos/adegadevinhos/domain/Vinho.java
@@ -1,5 +1,6 @@
 package adega.de.vinhos.adegadevinhos.domain;
 
+import adega.de.vinhos.adegadevinhos.util.TranslationConstants;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,6 +10,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
 
 @AllArgsConstructor
 @Data
@@ -20,5 +22,6 @@ public class Vinho {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotEmpty(message = TranslationConstants.TIPO_NOT_EMPTY_OR_NULL)
     private String tipo;
 }
---
+++ src/test/java/adega/de/vinhos/adegadevinhos/repository/VinhoRepositoryTest.java
@@ -0,0 +1,93 @@
+package adega.de.vinhos.adegadevinhos.repository;
+
+import adega.de.vinhos.adegadevinhos.domain.Vinho;
+import adega.de.vinhos.adegadevinhos.util.TranslationConstants;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import javax.validation.ConstraintViolationException;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class VinhoRepositoryTest {
+    @Autowired
+    private VinhoRepository vinhoRepository;
+
+    private Vinho vinhoToBeSaved;
+
+    @BeforeEach
+    void setUp(){
+        vinhoToBeSaved = createVinho();
+
+    }
+    @Test
+    void testSaveDevePersistirComSucesso(){
+        Vinho vinhoSaved = assertDoesNotThrow(() -> vinhoRepository.save(vinhoToBeSaved));
+        assertNotNull(vinhoSaved);
+        assertNotNull(vinhoSaved.getId());
+        assertNotNull(vinhoSaved.getTipo());
+        assertEquals(vinhoToBeSaved.getTipo(), vinhoSaved.getTipo());
+    }
+
+    @Test
+    void testPutDeveAtualizarComSucesso(){
+        Vinho vinhoToBeUpdated = assertDoesNotThrow(() -> vinhoRepository.save(vinhoToBeSaved));
+        assertNotNull(vinhoToBeUpdated);
+
+        vinhoToBeUpdated.setTipo("Tinto");
+        Vinho vinhoUpdated = assertDoesNotThrow(() -> vinhoRepository.save(vinhoToBeUpdated));
+
+        assertNotNull(vinhoUpdated);
+        assertNotNull(vinhoUpdated.getId());
+        assertNotNull(vinhoUpdated.getTipo());
+        assertEquals(vinhoToBeUpdated.getTipo(), vinhoUpdated.getTipo());
+    }
+
+    @Test
+    void testDeleteDeveDeletarComSucesso(){
+        Vinho vinhoToBeRemoved = assertDoesNotThrow(() -> vinhoRepository.save(vinhoToBeSaved));
+        assertNotNull(vinhoToBeRemoved);
+
+        assertDoesNotThrow(() -> vinhoRepository.delete(vinhoToBeRemoved));
+        Optional<Vinho> optionalVinho = vinhoRepository.findById(vinhoToBeRemoved.getId());
+
+        assertTrue(optionalVinho.isEmpty());
+    }
+
+    @Test
+    void testFindByTipoDeveRetornarUmaListaNaoVazia(){
+        Vinho vinhoSaved = assertDoesNotThrow(() -> vinhoRepository.save(vinhoToBeSaved));
+        assertNotNull(vinhoSaved);
+
+        List<Vinho> vinhosRetorned = assertDoesNotThrow(() -> vinhoRepository.findByTipo(vinhoSaved.getTipo()));
+        assertFalse(vinhosRetorned.isEmpty());
+        assertNotNull(vinhosRetorned.get(0));
+        assertEquals(vinhoSaved.getTipo(), vinhosRetorned.get(0).getTipo());
+    }
+
+    @Test
+    void testFindByTipoDeveRetornarUmaListaVazia(){
+        List<Vinho> vinhosRetorned = assertDoesNotThrow(() -> vinhoRepository.findByTipo("Tinto"));
+
+        assertTrue(vinhosRetorned.isEmpty());
+    }
+
+    @Test
+    void testSaveDeveLancarExcecao(){
+        vinhoToBeSaved.setTipo("");
+        Assertions.assertThatExceptionOfType(ConstraintViolationException.class)
+                .isThrownBy(() -> vinhoRepository.save(vinhoToBeSaved))
+                .withMessageContaining(TranslationConstants.TIPO_NOT_EMPTY_OR_NULL);
+    }
+
+    private Vinho createVinho(){
+        return Vinho.builder().tipo("Branco").build();
+    }
+
+}
