7133ed297d1a18b01c61277585285a4f781d2d27
vanderleik <vanderleik@yahoo.com.br>
2023-05-16 14:42:44 -0300

    update

--- target/generated-sources/annotations/adega/de/vinhos/adegadevinhos/mapper/AdegaMapperImpl.java
+++ target/generated-sources/annotations/adega/de/vinhos/adegadevinhos/mapper/AdegaMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-05-14T16:58:54-0300",
+    date = "2023-05-16T14:41:49-0300",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.2 (Oracle Corporation)"
 )
 @Component
